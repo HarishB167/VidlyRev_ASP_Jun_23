c5da7db0399f6bda632be6b18761d94c85b7650e
Hairsh <harishpoctor@gmail.com>
2023-06-12 08:45:06 +0530

    Building Forms - Exercise completed

--- VidlyRev/Models/Movie.cs
+++ VidlyRev/Models/Movie.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace VidlyRev.Models
 {
@@ -9,10 +11,18 @@ namespace VidlyRev.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number in Stock")]
         public int NumberInStock { get; set; }
+
         public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
     }
 }
